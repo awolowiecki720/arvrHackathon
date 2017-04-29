c3a210e2a60d6ef30ecdae609ef181e59e31e21a
Alex Wolowiecki <alwolowi@microsoft.com>
2017-04-29 15:47:59 -0700

    Air tap and gaze scripts

---
+++ CityBuilder/Assets/Scripts/AirTapSwag.cs
@@ -0,0 +1,30 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+public class AirTapSwag : MonoBehaviour
+{
+    public static bool AirTap = false;
+    public static GameObject GazeTarget;
+    public static Vector3 GazeTargetPos;
+
+    bool mWasPressed;
+
+    void LateUpdate()
+    {
+        // Update AirTap
+        bool isPressed = Input.GetMouseButton(0);
+        foreach (InteractionSourceState iss in InteractionManager.GetCurrentReading())
+        {
+            isPressed = iss.pressed;
+        }
+
+        AirTap = mWasPressed && !isPressed;
+        mWasPressed = isPressed;
+
+        // Update GazeTarget
+        GazeTarget = GazeManager.Instance.HitObject;
+        GazeTargetPos = GazeManager.Instance.HitPosition;
+
+    }
+}
---
+++ CityBuilder/Assets/Scripts/GazePlaceSwag.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using System;
+
+public class GazePlaceSwag : MonoBehaviour, IInputClickHandler
+{
+    bool pickedUp = false;
+    float lerpSpeed = 0.1f;
+    float mDist = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (AirTapSwag.AirTap)
+        {
+            pickedUp = true;
+        }
+
+        if (pickedUp)
+        {
+            Vector3 camPos = Camera.main.transform.position;
+            Vector3 pos = Vector3.zero;
+
+            pos = camPos + Camera.main.transform.forward * mDist;
+        }
+       
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        if (!pickedUp)
+            InputManager.Instance.PushModalInputHandler(this.gameObject);
+        else
+            InputManager.Instance.PopModalInputHandler();
+
+        TogglePickUpState();
+    }
+
+    private void TogglePickUpState()
+    {
+        pickedUp = !pickedUp;
+        mDist = Vector3.Distance(Camera.main.transform.position, this.transform.position);
+    }
+}
