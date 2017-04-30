743f8581d99c919e3b7345f96e2162239b998c3d
pksuira <andrektso@gmail.com>
2017-04-29 17:03:23 -0700

    added cubecount, highestYCoordinate to hud

---
+++ CityBuilder/Assets/CameraUpdater.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraUpdater : MonoBehaviour {
+
+	public Text numObjectsText;
+	public Text highestYCoordinateText;
+
+	private int numObjects;
+	private float highestYCoordinate;
+
+	// Use this for initialization
+	void Start () {
+		numObjects = 0;
+		highestYCoordinate = float.MinValue;
+		setNumObjectsText();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		updateNumObjects();
+		setNumObjectsText();
+		updateHighestYCoordinate();
+		setHighestYCoordinateText();
+	}
+
+	void updateHighestYCoordinate() {
+
+		highestYCoordinate = float.MinValue;
+
+		GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("cube");
+		foreach(GameObject gameObject in gameObjects) {
+			float y = gameObject.transform.position.y;
+			if (y > highestYCoordinate) {
+				highestYCoordinate = y;
+			}
+		}
+	}
+
+	void updateNumObjects() {
+		int count = 0;
+
+		GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("cube");
+		foreach(GameObject gameObject in gameObjects) {
+			if (gameObject.activeInHierarchy) {
+				count++;
+			}
+		}
+		numObjects = count;
+	}
+
+	void setHighestYCoordinateText() {
+		highestYCoordinateText.text = "Highest Y Coordinate: " + highestYCoordinate.ToString();
+	}
+
+	void setNumObjectsText() {
+		numObjectsText.text = "# Cubes: " + numObjects.ToString();
+	}
+}
