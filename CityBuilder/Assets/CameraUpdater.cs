35507016e6b3dae98d2a04d5d5435f9ff1b67e3e
pksuira <andrektso@gmail.com>
2017-04-29 17:51:03 -0700

    added position on hud

--- CityBuilder/Assets/CameraUpdater.cs
+++ CityBuilder/Assets/CameraUpdater.cs
@@ -7,15 +7,30 @@ public class CameraUpdater : MonoBehaviour {
 
 	public Text numObjectsText;
 	public Text highestYCoordinateText;
+	public Text didWinText;
+	public Text cameraText;
 
 	private int numObjects;
 	private float highestYCoordinate;
+	private bool hasWon = false;
+	private float cameraX;
+	private float cameraZ;
+
+	// y to win
+	private int maxY = 50;
 
 	// Use this for initialization
 	void Start () {
 		numObjects = 0;
-		highestYCoordinate = float.MinValue;
 		setNumObjectsText();
+		
+		highestYCoordinate = float.MinValue;
+		highestYCoordinateText.text = "";
+		
+		didWinText.text = "";
+
+		cameraX = 0.0f;
+		cameraZ = 0.0f;
 	}
 	
 	// Update is called once per frame
@@ -24,6 +39,9 @@ public class CameraUpdater : MonoBehaviour {
 		setNumObjectsText();
 		updateHighestYCoordinate();
 		setHighestYCoordinateText();
+		checkDidWin();
+		updateCameraCoordinates();
+		setCameraCoordinatesText();
 	}
 
 	void updateHighestYCoordinate() {
@@ -32,9 +50,11 @@ public class CameraUpdater : MonoBehaviour {
 
 		GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("cube");
 		foreach(GameObject gameObject in gameObjects) {
-			float y = gameObject.transform.position.y;
-			if (y > highestYCoordinate) {
-				highestYCoordinate = y;
+			if (gameObject.activeInHierarchy) {
+				float y = gameObject.transform.position.y;
+				if (y > highestYCoordinate) {
+					highestYCoordinate = y;
+				}
 			}
 		}
 	}
@@ -51,11 +71,30 @@ public class CameraUpdater : MonoBehaviour {
 		numObjects = count;
 	}
 
+	void checkDidWin() {
+		if (highestYCoordinate > maxY && !hasWon) {
+			setDidWinText();
+		}
+	}
+
+	void updateCameraCoordinates() {
+		cameraX = Camera.main.gameObject.transform.position.x;
+		cameraZ = Camera.main.gameObject.transform.position.z;
+	}
+
 	void setHighestYCoordinateText() {
 		highestYCoordinateText.text = "Highest Y Coordinate: " + highestYCoordinate.ToString();
 	}
 
 	void setNumObjectsText() {
 		numObjectsText.text = "# Cubes: " + numObjects.ToString();
+	}
+
+	void setDidWinText() {
+		didWinText.text = "YOU WON!!";
+	}
+
+	void setCameraCoordinatesText() {
+		cameraText.text = "position: " + ((int)cameraX).ToString() + ", " + ((int)cameraZ).ToString();
 	}
 }
